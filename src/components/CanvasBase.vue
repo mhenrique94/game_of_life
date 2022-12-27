eb66d2a30c024e5680ffa436ec11e5e9c8344d66
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2022-12-27 09:27:46 -0300

    Adiciona interação inicial com click

--- src/components/CanvasBase.vue
+++ src/components/CanvasBase.vue
@@ -2,11 +2,7 @@
   <div class="canvasBase">
     <div v-for="cellX in width" :key="cellX">
       <div v-for="cellY in height" :key="cellY">
-        <SingleCell
-          :key="`${cellX}:${cellY}`"
-          :x="cellX"
-          :y="cellY"
-        ></SingleCell>
+        <SingleCell :key="`${cellX}:${cellY}`" />
       </div>
     </div>
   </div>
@@ -19,19 +15,13 @@ export default {
   components: { SingleCell },
   data() {
     return {
-      cellsArray: [],
       boardSize: 0,
       width: 12,
       height: 12,
     };
   },
   created() {
-    this.boardSize = this.cellsCount();
-  },
-  methods: {
-    cellsCount() {
-      return this.width * this.height;
-    },
+    this.boardSize = this.width * this.height;
   },
 };
 </script>
@@ -40,7 +30,8 @@ export default {
 .canvasBase {
   border: 1px solid grey;
   background-color: #63b984;
-  width: 384px;
   display: flex;
+  padding: 32px;
+  margin: 32px;
 }
 </style>
--- src/components/SingleCell.vue
+++ src/components/SingleCell.vue
@@ -1,15 +1,38 @@
 <template>
-  <button name="cell" class="cellButton" :cellId="`${cellX}:{cellY}`"></button>
+  <button
+    name="cell"
+    class="cellButton"
+    :class="{ active: isActive }"
+    :key="key"
+    @click="toggleAlive(cellId)"
+  ></button>
 </template>
 <script>
 export default {
   name: "SingleCell",
-  props: ["cellX", "cellY"],
+  data() {
+    return {
+      isActive: 0,
+    };
+  },
+  methods: {
+    toggleAlive(cellId) {
+      this.isActive = this.isActive ? this.isActive - 1 : this.isActive + 1;
+      this.$emit("toggleAlive", cellId);
+    },
+  },
+  computed: {},
 };
 </script>
 <style scoped>
 .cellButton {
   width: 32px;
   height: 32px;
+  margin: 1px;
+  line-height: 0;
+}
+
+.active {
+  background-color: black;
 }
 </style>
